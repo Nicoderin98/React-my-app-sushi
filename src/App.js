3c5ad98299375bf3a184046496c934290649d61d
Nicolo De Rinaldis <nicoderin98@gmail.com>
2022-07-19 07:47:57 +0200

    update quantità

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import shrimp from './images/shrimp.png'
 class App extends Component {
   state = {
     cards:[
-      {id:0,nome:"California",prezzo:1.99,immagine:california},
+      {id:0,nome:"California",prezzo:1.99,immagine:california, quantità:0},
       {id:1,nome:"Dragon",prezzo:3.49,immagine:dragon,quantità:0},
       {id:2,nome:"Dynamite",prezzo:2.49,immagine:dynamite,quantità:0},
       {id:3,nome:"Philadelphia",prezzo:0.99,immagine:philadelphia,quantità:0},
@@ -25,6 +25,13 @@ class App extends Component {
     this.setState({ cards });
   }
 
+  handleIncrement = card => {
+    const cards = [...this.state.cards];
+    const id = cards.indexOf(card);
+    cards[id] = {...card};
+    cards[id].quantità++;
+    this.setState({cards});
+  }
   render() {
   return (
     <>
@@ -37,6 +44,7 @@ class App extends Component {
           <Card
           key={card.id}
           onDelete={this.handleDelete}
+          onIncrement={this.handleIncrement}
           card={card}/>
         ))}
         </div>
--- src/components/card.js
+++ src/components/card.js
@@ -5,6 +5,8 @@ export default class card extends Component {
   return (
     <div className='col'>
       <div className="card" style={{width: "18rem", textAlign:"center"}}>
+      <button onClick={() => this.props.onIncrement(this.props.card)} className="btn btn-primary">
+        Aggiungi <span className='badge badge-light'>{this.props.card.quantità}</span></button>
         <img src={this.props.card.immagine} class="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{this.props.card.nome} Roll</h5>
